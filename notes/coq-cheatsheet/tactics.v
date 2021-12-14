2351ef9d422b2d5f5b3b612f2cc37c265cd3c8ae
julesjacobs <julesjacobs@gmail.com>
2021-12-14 12:19:47 +0300

    Tactics

---
+++ notes/coq-cheatsheet/tactics.v
@@ -0,0 +1,15 @@
+Tactic Notation "simplify_eq" := repeat
+  match goal with
+  | H : ?f _ = ?f _ |- _ => progress injection H as H
+  | H : ?f _ _ = ?f _ _ |- _ => progress injection H as H
+  | H : ?f _ _ _ = ?f _ _ _ |- _ => progress injection H as H
+  | H : ?f _ _ _ _ = ?f _ _ _ _ |- _ => progress injection H as H
+  | H : ?f _ _ _ _ _ = ?f _ _ _ _ _ |- _ => progress injection H as H
+  | H : ?f _ _ _ _ _ _ = ?f _ _ _ _ _ _ |- _ => progress injection H as H
+  | H : ?x = ?x |- _ => clear H
+  | H1 : ?o = Some ?x, H2 : ?o = Some ?y |- _ =>
+    assert (y = x) by congruence; clear H2
+  | _ => congruence || (progress subst)
+  end.
+
+Ltac inv H := inversion H; clear H; simplify_eq.
