d7f3b730aba5a4d605608bd49ff7f3080a35fae8
julesjacobs <julesjacobs@gmail.com>
2025-05-14 00:43:38 -1000

    x

---
+++ misc/katch2/pkg/katch2_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const greet: (a: number, b: number) => [number, number];
+export const analyze_expression: (a: number, b: number) => any;
+export const init_panic_hook: () => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __wbindgen_start: () => void;
