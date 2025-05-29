09f3fba4e3e81776610f5f2ecaecf8f7027529e8
julesjacobs <julesjacobs@gmail.com>
2025-05-29 17:42:38 -0400

    Update KATch2 tutorial in misc/katch2 - 2025-05-29 17:42:38

--- misc/katch2/katch2ui/katch2-editor.js
+++ misc/katch2/katch2ui/katch2-editor.js
@@ -181,7 +181,7 @@ class KATch2Editor {
         // Define syntax highlighting
         monaco.languages.setMonarchTokensProvider('netkat', {
             keywords: ['dup', 'T', 'X', 'U', 'F', 'G', 'R', 'if', 'then', 'else', 'let', 'in'],
-            operators: [':=', '==', '+', '&', '^', '-', '~', '!', ';', '*', '..'],
+            operators: [':=', '==', '+', '&', '^', '-', '~', '!', ';', '*', '·', '..'],
             symbols: /[=><!~?:&|+\-*\/\^%;()]+/,
 
             tokenizer: {
@@ -1289,7 +1289,7 @@ class KATch2Editor {
                 i += 2;
                 matched = true;
             }
-            else if (['+', '&', '^', '-', '~', ';', '*'].includes(code[i])) {
+            else if (['+', '&', '^', '-', '~', ';', '*', '·'].includes(code[i])) {
                 tokens.push({ type: 'operator', text: code[i] });
                 i++;
                 matched = true;
